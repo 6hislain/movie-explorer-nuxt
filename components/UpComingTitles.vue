bd3e6f83674245b87937ef7a107493d7d844bcdb
6hislain <ghislainmike80@gmail.com>
2023-11-18 05:14:06 +0200

    .

--- components/UpComingTitles.vue
+++ components/UpComingTitles.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row>
-    <v-col cols="12" md="3">
+    <v-col cols="12" md="3" v-if="items.length > 11">
       <v-row>
         <v-col cols="12">
           <v-card>
@@ -32,13 +32,13 @@
       <v-carousel show-arrows="hover" interval="5000" continuous cycle>
         <v-carousel-item
           :src="item?.primaryImage?.url"
-          cover
           v-for="(item, id) in items"
           :key="id"
+          cover
         ></v-carousel-item>
       </v-carousel>
     </v-col>
-    <v-col cols="12" md="3">
+    <v-col cols="12" md="3" v-if="items.length > 11">
       <v-row>
         <v-col cols="12">
           <v-card>
