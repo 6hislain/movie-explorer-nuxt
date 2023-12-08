8abf4db637a6b8e58789a990bcec496c1b507255
6hislain <ghislainmike80@gmail.com>
2023-12-08 21:36:27 +0200

    box office titles

--- components/BoxOfficeTitles.vue
+++ components/BoxOfficeTitles.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row>
-    <v-col cols="8">
+    <v-col cols="12" md="8">
       <v-carousel show-arrows="hover" interval="5000" continuous cycle>
         <v-carousel-item
           :src="item?.primaryImage?.url"
@@ -9,6 +9,16 @@
           cover
         ></v-carousel-item>
       </v-carousel>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-list>
+        <v-list-item
+          :key="id"
+          v-for="(item, id) in items"
+          :title="item?.titleText?.text"
+          :subtitle="item?.releaseYear?.year"
+        />
+      </v-list>
     </v-col>
   </v-row>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,27 @@
 <template>
-  <v-row no-gutters>
-    <v-col md="12"><Genres :genres="genresRequest.data" /></v-col>
-    <v-col md="12"><UpComingTitles :titles="upcomingRequest.data" /></v-col>
-    <v-col md="12">
-      <h4 class="text-h4 my-3">Movies</h4>
-      <RandomTitles :titles="randomRequest.data" />
-    </v-col>
-    <v-col md="12">
-      <h4 class="text-h4 my-3">Series</h4>
-      <RandomTitles :titles="randomSeriesRequest.data" />
-    </v-col>
-  </v-row>
+  <div>
+    <Head>
+      <Title>Home - Movie Explorer Nuxt</Title>
+      <Meta
+        name="description"
+        content="Welcome to our Movie Explorer project developed using Nuxt.js!"
+      />
+    </Head>
+    <v-row no-gutters>
+      <v-col cols="12"><Genres :genres="genresRequest.data" /></v-col>
+      <v-col cols="12"
+        ><BoxOfficeTitles :titles="boxOfficeRequest.data"
+      /></v-col>
+      <v-col cols="12">
+        <h4 class="text-h4 my-3">Movies</h4>
+        <GroupedTitles :titles="movieRequest.data" />
+      </v-col>
+      <v-col cols="12">
+        <h4 class="text-h4 my-3">Series</h4>
+        <GroupedTitles :titles="seriesRequest.data" />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -24,15 +35,15 @@ const genresRequest = await useFetch(
   runtimeConfig.public.API_URL + "/titles/utils/genres",
   { headers }
 );
-const upcomingRequest = await useFetch(
-  runtimeConfig.public.API_URL + "/titles/x/upcoming",
-  { headers, query: { limit: 15 } }
+const boxOfficeRequest = await useFetch(
+  runtimeConfig.public.API_URL + "/titles/random",
+  { headers, query: { limit: 10, list: "top_boxoffice_last_weekend_10" } }
 );
-const randomRequest = await useFetch(
+const movieRequest = await useFetch(
   runtimeConfig.public.API_URL + "/titles/random",
   { headers, query: { limit: 20, list: "top_boxoffice_200" } }
 );
-const randomSeriesRequest = await useFetch(
+const seriesRequest = await useFetch(
   runtimeConfig.public.API_URL + "/titles/random",
   { headers, query: { limit: 20, list: "top_rated_series_250" } }
 );
