6d4e173c5493eb9adef88ca5082ffad37070b329
6hislain <ghislainmike80@gmail.com>
2023-12-08 21:51:15 +0200

    triple carousel

--- components/BoxOfficeTitles.vue
+++ components/BoxOfficeTitles.vue
@@ -1,31 +1,43 @@
 <template>
   <v-row>
-    <v-col cols="12" md="8">
-      <v-carousel show-arrows="hover" interval="5000" continuous cycle>
+    <v-col cols="12" md="4">
+      <v-carousel show-arrows="hover" interval="8000" continuous cycle>
         <v-carousel-item
           :src="item?.primaryImage?.url"
-          v-for="(item, id) in items"
+          v-for="(item, id) in leftItems"
           :key="id"
           cover
         ></v-carousel-item>
       </v-carousel>
     </v-col>
     <v-col cols="12" md="4">
-      <v-list>
-        <v-list-item
+      <v-carousel show-arrows="hover" interval="7000" continuous cycle>
+        <v-carousel-item
+          :src="item?.primaryImage?.url"
+          v-for="(item, id) in centerItems"
           :key="id"
-          v-for="(item, id) in items"
-          :title="item?.titleText?.text"
-          :subtitle="item?.releaseYear?.year"
-        />
-      </v-list>
+          cover
+        ></v-carousel-item>
+      </v-carousel>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-carousel show-arrows="hover" interval="8000" continuous cycle>
+        <v-carousel-item
+          :src="item?.primaryImage?.url"
+          v-for="(item, id) in rightItems"
+          :key="id"
+          cover
+        ></v-carousel-item>
+      </v-carousel>
     </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
 const props = defineProps(["titles"]);
-const items = ref(props?.titles?._value?.results);
+const leftItems = ref([...props?.titles?._value?.results].slice(0, 3));
+const rightItems = ref([...props?.titles?._value?.results].slice(3, 6));
+const centerItems = ref([...props?.titles?._value?.results].slice(6, 10));
 </script>
 
 <style scoped>
